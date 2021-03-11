fb567d7cba67cbfb98316bb052e9089761eb5ce6
Mansi-Atey <mansiatey31@gmail.com>
2021-03-11 20:38:49 +0530

    [mansi]ADDED: enter valid last name

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+   public class Program
+    {
+       public static void Main(string[] args)
+        {
+            Regex regexValidation = new Regex();
+            Console.WriteLine("Enter Your First Name");
+            string FirstName = Console.ReadLine();
+            Console.WriteLine(regexValidation.validateFirstName(FirstName));
+            Console.WriteLine("Enter Your Last Name");
+            string LastName = Console.ReadLine();
+            Console.WriteLine(regexValidation.validateLastName(LastName));
+        }
+    }
+}
