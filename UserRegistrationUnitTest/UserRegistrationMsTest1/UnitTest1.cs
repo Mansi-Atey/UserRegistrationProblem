36ec30ad297b6135edd6a14a81523fd47e8bcfd9
Mansi-Atey <78947536+Mansi-Atey@users.noreply.github.com>
2021-03-19 01:07:47 +0530

    Update UnitTest1.cs

--- UserRegistrationUnitTest/UserRegistrationMsTest1/UnitTest1.cs
+++ UserRegistrationUnitTest/UserRegistrationMsTest1/UnitTest1.cs
@@ -9,6 +9,106 @@ namespace UserRegistrationMsTest1
         [TestMethod]
         public void TestMethod1()
         {
+            private RegexValidation user;
+
+        [TestInitialize]
+        public void InitClassObject()
+        {
+            //Arrange
+            user = new RegexValidation();
+        }
+        [TestMethod]
+        public void GivenUserFistName_WhenValidate_ShouldReturnTrue()
+        {
+            string firstName = "anil";
+            //Act
+            bool result = user.ValidateFirstName(firstName);
+            //Assert
+            Assert.IsTrue(result);
+        }
+        [TestMethod]
+        public void GivenUserLastName_WhenValidate_ShouldReturnTrue()
+        {
+            string lastName = "kumar";
+            //Act
+            bool result = user.ValidateFirstName(lastName);
+            //Assert
+            Assert.IsTrue(result);
+        }
+        [TestMethod]
+        public void GivenEmailId_WhenValidate_ShouldReturnTrue()
+        {
+            string email = "anilkumar@gmail.com";
+            //Act
+            bool result = user.ValidateEmail(email);
+            //Assert
+            Assert.IsTrue(result);
+        }
+        [TestMethod]
+        public void GivenMobileNumber_WhenValidate_ShouldReturnTrue()
+        {
+            string mobileNumber = "91 98766942446";
+            //Act
+            bool result = user.ValidateMobile(mobileNumber);
+            //Assert
+            Assert.IsTrue(result);
+        }
+        [TestMethod]
+        public void GivenPassword_WhenValidate_ShouldReturnTrue()
+        {
+            //Arrange
+
+            string password = "ani#732";
+            //Act
+            bool result = user.ValidatePassword(password);
+            //Assert
+            Assert.IsTrue(result);
+        }
+        //Sad Test Cases(Test Cases Fail The Entry)
+        [TestMethod]
+        public void GivenUserFistName_WhenValidate_ShouldReturnFalse()
+        {
+            string firstName = "anil";
+            //Act
+            bool result = user.ValidateFirstName(firstName);
+            //Assert
+            Assert.IsFalse(result);
+        }
+        [TestMethod]
+        public void GivenUserLastName_WhenValidate_ShouldReturnFalse()
+        {
+            string lastName = "kumar";
+            //Act
+            bool result = user.ValidateFirstName(lastName);
+            //Assert
+            Assert.IsFalse(result);
+        }
+        [TestMethod]
+        public void GivenEmailId_WhenValidate_ShouldReturnFalse()
+        {
+            string email = "anil.kumar.com";
+            //Act
+            bool result = user.ValidateEmail(email);
+            //Assert
+            Assert.IsFalse(result);
+        }
+        [TestMethod]
+        public void GivenMobileNumber_WhenValidate_ShouldReturnFalse()
+        {
+            string mobileNumber = "91 0876543210";
+            //Act
+            bool result = user.ValidateMobile(mobileNumber);
+            //Assert
+            Assert.IsFalse(result);
+        }
+        [TestMethod]
+        public void GivenPassword_WhenValidate_ShouldReturnFalse()
+        {
+            string password = "anil@3*";
+            //Act
+            bool result = user.ValidatePassword(password);
+            //Assert
+            Assert.IsFalse(result);
         }
     }
 }
