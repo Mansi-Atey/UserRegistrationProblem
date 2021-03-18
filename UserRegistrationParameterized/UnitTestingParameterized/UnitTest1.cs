f1a5c0a58cee01c92fb94e112616d0f0b8d900c8
Mansi-Atey <78947536+Mansi-Atey@users.noreply.github.com>
2021-03-19 01:05:36 +0530

    Update UnitTest1.cs

--- UserRegistrationParameterized/UnitTestingParameterized/UnitTest1.cs
+++ UserRegistrationParameterized/UnitTestingParameterized/UnitTest1.cs
@@ -9,6 +9,120 @@ namespace UnitTestingParameterized
         [TestMethod]
         public void TestMethod1()
         {
+             private RegexValidation user;
+
+        [TestInitialize]
+        public void InitClassObject()
+        {
+            //Arrange
+            user = new RegexValidation();
+        }
+        [TestMethod]
+        public void GivenUserFistName_WhenValidate_ShouldReturnTrue()
+        {
+            string firstName = "anil";
+            //Act
+            bool result = user.ValidateFirstName(firstName);
+            //Assert
+            Assert.IsTrue(result);
+        }
+        [TestMethod]
+        public void GivenUserLastName_WhenValidate_ShouldReturnTrue()
+        {
+            string lastName = "kumar";
+            //Act
+            bool result = user.ValidateFirstName(lastName);
+            //Assert
+            Assert.IsTrue(result);
+        }
+        [TestMethod]
+        public void GivenEmailId_WhenValidate_ShouldReturnTrue()
+        {
+            string email = "anlkumar97@gmail.com";
+            //Act
+            bool result = user.ValidateEmail(email);
+            //Assert
+            Assert.IsTrue(result);
+        }
+        [TestMethod]
+        public void GivenMobileNumber_WhenValidate_ShouldReturnTrue()
+        {
+            string mobileNumber = "91 9138642446";
+            //Act
+            bool result = user.ValidateMobile(mobileNumber);
+            //Assert
+            Assert.IsTrue(result);
+        }
+        [TestMethod]
+        public void GivenPassword_WhenValidate_ShouldReturnTrue()
+        {
+            //Arrange
+
+            string password = "anil2652";
+            //Act
+            bool result = user.ValidatePassword(password);
+            //Assert
+            Assert.IsTrue(result);
+        }
+        //Sad Test Cases(Test Cases Fail The Entry)
+        [TestMethod]
+        public void GivenUserFistName_WhenValidate_ShouldReturnFalse()
+        {
+            string firstName = "anil";
+            //Act
+            bool result = user.ValidateFirstName(firstName);
+            //Assert
+            Assert.IsFalse(result);
+        }
+        [TestMethod]
+        public void GivenUserLastName_WhenValidate_ShouldReturnFalse()
+        {
+            string lastName = "kumar";
+            //Act
+            bool result = user.ValidateFirstName(lastName);
+            //Assert
+            Assert.IsFalse(result);
+        }
+        [TestMethod]
+        public void GivenEmailId_WhenValidate_ShouldReturnFalse()
+        {
+            string email = "anilkumar.com";
+            //Act
+            bool result = user.ValidateEmail(email);
+            //Assert
+            Assert.IsFalse(result);
+        }
+        [TestMethod]
+        public void GivenMobileNumber_WhenValidate_ShouldReturnFalse()
+        {
+            string mobileNumber = "91 0878566210";
+            //Act
+            bool result = user.ValidateMobile(mobileNumber);
+            //Assert
+            Assert.IsFalse(result);
+        }
+        [TestMethod]
+        public void GivenPassword_WhenValidate_ShouldReturnFalse()
+        {
+            string password = "anil3@4";
+            //Act
+            bool result = user.ValidatePassword(password);
+            //Assert
+            Assert.IsFalse(result);
+        }
+        [TestMethod]
+        [DataRow("abc@yahoo.com")]
+        [DataRow("abc-100@yahoo.com")]
+        [DataRow("abc.100@yahoo.com")]
+        [DataRow("abc111@abc.com")]
+        [DataRow("abc.100@abc.com.au")]
+        [DataRow("abc-100@abc.net")]
+        [DataRow("abc@1.com")]
+        [DataRow("abc@gmail.com.com")]
+        [DataRow("abc+100@gmail.com")]
+        public void ValidateEmailId_Should_return_true(string email)
+        {
+            Assert.IsTrue(user.ValidateEmail(email));
         }
     }
 }
